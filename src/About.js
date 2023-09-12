85c25313d06d36005bb11104bc92640c6dd6e402
lilflyy <Qyf1314>
2023-09-12 12:42:23 -0400

    new changes icon added

--- src/About.js
+++ src/About.js
@@ -1,5 +1,10 @@
 import pic from './profile.JPG'
+import school from './purdue.svg'
 const About = () => {
+    const openInNewTab = (url) => {
+        const newWindow = window.open(url, '_blank', 'noopener,noreferrer')
+        if (newWindow) newWindow.opener = null
+    }
     return ( 
         <section className="about">
       <p class="section__text__p1">Get To Know More</p>
@@ -20,9 +25,17 @@ const About = () => {
             </div>
           </div>
           <div class="text-container">
-            <p>
+            
+            <div>
+                 <p>
             I am Yufei Qiu, a junior in Purdue University, majoring in Computer Science with a Mathematics minor. Always looking forward to learn new things!
             </p>
+            </div>
+            <div> 
+               
+                    <img src={school} className="icon" onClick={() => { openInNewTab("https://www.purdue.edu") }}/>
+              
+            </div>
           </div>
         </div>
       </div>
